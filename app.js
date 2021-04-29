12fc09a856b82eeffe50aeb80e360fbe03a321d2
zyf <freeman.zheng@zhan.com>
2021-04-29 17:39:56 +0800

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+App({
+    onLaunch() {
+        
+    },
+    globalData: {
+        userInfo: {}, // 用户信息
+        path: '', // 未登录状态tab页面路径
+        shouldUploadList: false, // 是否更新页面数据
+        appid: 'wxcba5ee2287bfbe48',
+        listItem: {},
+        resultData: {},
+        videoItem: ""
+    }
+});
